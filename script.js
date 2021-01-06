a2e23735e57b51843e62dcdf9cb72bbe182ddec7
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-01-05 19:28:14 -0500

    added strings to uppercase, lowercase, special character and number

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
-var lowerCase = ["a","b",];
-var upperCase = ["A","B",];
-var specialCharacter = ["!"];
-var number = ["1","2",]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCharacter = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","|"];
+var number = ["1","2","3","4","5","6","7","8","9","0"];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -55,7 +55,7 @@ function generatePassword(){
   // Create arr that stores final password
   // Create arr that stores possible characters
   // Create arr that stores garaunteed characters used
-  // 4 Conditional statements that check if values from object are true (options.hasSpecialCharacters){
+  // 4 Conditional statements that check if values from object are true (options.hasSpecialCharacters)
     // combine possible characters with special characters
     // call getRandomFunction, pass the arr (special characters arr)
     // push random character into specialCharacters arr x4 (num, upper, lower)
